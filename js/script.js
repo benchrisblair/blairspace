de8c07da0d1206941fbe72cfa786cca7b97ff5f1
benchrisblair <benblair@Bens-MacBook-Pro.local>
2015-08-24 08:17:45 -0700

    Structural and content changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+// 	$(window).on('scroll', function() {
+// 		var scrollPosition = window.scrollY;
+		
+// 		if (scrollPosition >= 800) {
+// 			$('.navbar').removeClass('navbar-static-top');
+// 			$('.navbar').addClass('navbar-fixed-top');
+// 		} else {
+// 			$('.navbar').removeClass('navbar-fixed-top');
+// 			$('.navbar').addClass('navbar-static-top');
+// 		}
+// 	});
+// });
